936f63b9904cd6722e83305cd65c317a4e5cff20
aguroshou <aguroshou0413@gmail.com>
2021-10-22 17:26:38 +0900

    add: 途中まで編集していたUnityプロジェクトを追加しました。

---
+++ DanosuUnityProject/Assets/Aguro/DanceGameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceGameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject LuneAvater;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuneAvater.transform.localScale = Vector3.one*4.0f;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
