dd27de6adb0b5440d19eaf03bed6e08835fbf8a3
KazuXlaws <machinedoll.kazu@gmail.com>
2021-10-26 20:13:05 +0900

    add: Great, Good判定時のSEを変更できるようにしました add: 効果音を追加しました

--- DanosuUnityProject/Assets/esaki/Scripts/SoundManager.cs
+++ DanosuUnityProject/Assets/esaki/Scripts/SoundManager.cs
@@ -20,6 +20,8 @@ public class SoundManager : MonoBehaviour
     private int backIndex;
     private int selectIndex;
     private int playIndex;
+    private int greatIndex;
+    private int goodIndex;
 
     private KindOfSound kindOfSound;
     private int selectedIndex;
@@ -38,6 +40,8 @@ public class SoundManager : MonoBehaviour
         backIndex = PlayerPrefs.GetInt("BackSoundIndex");
         selectIndex = PlayerPrefs.GetInt("SelectSoundIndex");
         playIndex = PlayerPrefs.GetInt("PlaySoundIndex");
+        greatIndex = PlayerPrefs.GetInt("GreatSoundIndex");
+        goodIndex = PlayerPrefs.GetInt("GoodSoundIndex");
 
         kindOfSound = KindOfSound.Transition;
         selectedIndex = -1;
@@ -62,6 +66,24 @@ public class SoundManager : MonoBehaviour
     {
         audioSource.clip = sounds[playIndex];
         audioSource.Play();
+    }
+    public void GreatSE()
+    {
+        audioSource.clip = sounds[greatIndex];
+        audioSource.Play();
+    }
+    public void GoodSE()
+    {
+        audioSource.clip = sounds[goodIndex];
+        audioSource.Play();
+    }
+    public void ChangeGreatSE(int i)
+    {
+        PlayerPrefs.SetInt("GreatSoundIndex", i);
+    }
+    public void ChangeGoodSE(int i)
+    {
+        PlayerPrefs.SetInt("GoodSoundIndex", i);
     }
 
     public void PlaySound(int i)
